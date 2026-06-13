7328ba2640fdab196d4abed1295706a80ae687a3
HenryMiles3 <henry6miles@gmail.com>
2026-06-13 18:15:10 +0100

    Creative mode tab changes
    Signed-off-by: HenryMiles3 <henry6miles@gmail.com>

--- src/main/java/com/nightmare/gooncraft/item/ModCreativeModeTabs.java
+++ src/main/java/com/nightmare/gooncraft/item/ModCreativeModeTabs.java
@@ -4,6 +4,7 @@ import com.nightmare.gooncraft.GoonCraft;
 import com.nightmare.gooncraft.block.ModBlocks;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 import net.neoforged.bus.api.IEventBus;
@@ -14,20 +15,23 @@ import java.util.function.Supplier;
 public class ModCreativeModeTabs {
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TAB = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, GoonCraft.MOD_ID);
 
-    public static final Supplier<CreativeModeTab> GOONCRAFT_TAB = CREATIVE_MODE_TAB.register("gooncrafttab",
+    public static final Supplier<CreativeModeTab> GOONCRAFT_ITEMS_TAB = CREATIVE_MODE_TAB.register("gooncraftitemtab",
             () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.CUMBUCKET.get()))
-                    .title(Component.translatable("creativetab.nightmaregooncraft.gooncrafttab"))
+                    .withTabsAfter(ResourceLocation.fromNamespaceAndPath(GoonCraft.MOD_ID, "gooncraftblocktab"))
+                    .title(Component.translatable("creativetab.nightmaregooncraft.gooncraftitemtab"))
                     .displayItems((itemDisplayParameters, output) -> {
-                        //Items
                         output.accept(ModItems.CUMBUCKET);
                         output.accept(ModItems.TYRONEBUCKET);
                         output.accept(ModItems.RAWCUM);
+                    }).build());
 
-                        //Blocks
+    public static final Supplier<CreativeModeTab> GOONCRAFT_BLOCKS_TAB = CREATIVE_MODE_TAB.register("gooncraftblocktab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModBlocks.HARDCUM.get()))
+                    .title(Component.translatable("creativetab.nightmaregooncraft.gooncraftblocktab"))
+                    .displayItems((itemDisplayParameters, output) -> {
                         output.accept(ModBlocks.HARDCUM);
                         output.accept(ModBlocks.PRIDEFLAG);
                         output.accept(ModBlocks.HARDCUMORE);
-
                     }).build());
 
 
